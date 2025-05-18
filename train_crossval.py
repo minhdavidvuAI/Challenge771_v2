b7fb66a6cc6e271babb253d00bdc1f9cf99e757d
David Vu <ai24m035@technikum-wien.at>
2025-05-18 23:55:13 +0200

    fixed syntax

--- train_crossval.py
+++ train_crossval.py
@@ -184,9 +184,8 @@ if __name__ == "__main__":
             
             #augmented_dataset = ESC50(root=augment_path, subset="train", test_folds={test_fold}, global_mean_std=global_stats[test_fold - 1], augmentedFlag=True)
             if not os.path.exists(augment_path):
-                audio_augmenter = AudioAugmenter(config.esc50_preprocessed, augment_path)
+                audio_augmenter = AudioAugmenter(os.path.join(config.esc50_preprocessed,'/fold_',testfold,'_train'), os.path.join(config.augment_path,'/fold_',testfold,'_train'))
                 audio_augmenter.augment_data()
-                
                 
             augmented_set = get_fold_augmented(subset="train")
             combined_dataset = ConcatDataset([train_set, augmented_set])
